dfe1ff31b975048968bf76b3c996cb24c2db0f5e
Ingo Kresse <kresse@in.tum.de>
2012-12-07 11:18:36 +0100

    removed a deprecation warning.
    tinyxml is now a system dependency.

--- loopback_controller_manager.cpp
+++ loopback_controller_manager.cpp
@@ -37,7 +37,7 @@
 #include "pr2_hardware_interface/hardware_interface.h"
 #include "pr2_controller_manager/controller_manager.h"
 #include "pr2_mechanism_model/robot.h"
-#include "tinyxml/tinyxml.h"
+#include "tinyxml.h"
 
 #include <XmlRpcValue.h>
 #include <XmlRpcException.h>
