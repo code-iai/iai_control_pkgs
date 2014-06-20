06014e6f28c6fb6c7c78d9e08ad4f3d52121a229
jannikb <jannikbu@tzi.de>
2014-06-20 17:22:12 +0200

    added comment in rename_topic.py

--- iai_rosbag_utils/src/rename_topic.py
+++ iai_rosbag_utils/src/rename_topic.py
@@ -2,8 +2,9 @@
 
 import rospy
 import sys
-
 from rosbag import Bag
+
+# This script takes 4 parameters in the following order: old-bag-file old-topic new-bag-file new-topic 
 with Bag(sys.argv[3], 'w') as Y:
     for topic, msg, t in Bag(sys.argv[1]):
         Y.write(sys.argv[4] if topic == sys.argv[2] else topic, msg, t)
