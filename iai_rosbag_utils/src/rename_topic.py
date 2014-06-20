f05fbe8453c37fd7a10d310a95289e2a97747343
Jannik Buckelo <jannikbu@tzi.de>
2014-06-20 15:29:30 +0200

    Added a check to the python script that prevents you from destroying your bag file if the name of both files are the same.

--- iai_rosbag_utils/src/rename_topic.py
+++ iai_rosbag_utils/src/rename_topic.py
@@ -2,8 +2,11 @@
 
 import rospy
 import sys
-
 from rosbag import Bag
+
+if sys.argv[1] == sys.argv[3]:
+	sys.exit("The new file needs to have another name.")
+
 with Bag(sys.argv[3], 'w') as Y:
     for topic, msg, t in Bag(sys.argv[1]):
         Y.write(sys.argv[4] if topic == sys.argv[2] else topic, msg, t)
