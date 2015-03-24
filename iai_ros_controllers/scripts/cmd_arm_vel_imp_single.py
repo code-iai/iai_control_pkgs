fc693c8c4c2413bcb986fcdfa72e136907303873
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2015-03-24 16:23:38 +0100

    Fixed testing scripts.

--- iai_ros_controllers/scripts/cmd_arm_vel_imp_single.py
+++ iai_ros_controllers/scripts/cmd_arm_vel_imp_single.py
@@ -9,11 +9,13 @@ from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-v", "--velocity", dest="velocity",
-                  help="Velocity commanded to each joint (VELOCITY) Default=%default (rads/s)", metavar="VELOCITY", default= 0.0)
+                  help="Velocity commanded to the joint (VELOCITY) Default=%default (rads/s)", metavar="VELOCITY", default= 0.0)
 parser.add_option("-s", "--stiffness", dest="stiffness",
                   help="Stiffness commanded to each joint (STIFFNESS) Default=%default (Nm/rad)", metavar="STIFFNESS", default= 80.0)
 parser.add_option("-d", "--damping", dest="damping",
                   help="Damping commanded to each joint (DAMPING) Default=%default", metavar="DAMPING", default= 0.7)
+parser.add_option("-n", "--number", dest="number",
+                  help="Joint number commanded (NUMBER) Default=%default", metavar="NUMBER", default= 6)
 parser.add_option("-t", "--topic", dest="topic",
                   help="Command topic Default=%default", metavar="TOPIC", default="/r_arm_vel/command")
 
@@ -27,7 +29,8 @@ def main():
 
     cmd = MultiJointVelocityImpedanceCommand()
     cmd.header.stamp = rospy.Time.now()
-    cmd.velocity = [ options.velocity ] * 7
+    cmd.velocity = [ 0.0 ] * 7
+    cmd.velocity[ int(options.number) ] = float(options.velocity)
     cmd.stiffness = [ options.stiffness ] * 7
     cmd.damping = [ options.damping ] * 7
 
@@ -35,10 +38,13 @@ def main():
     print options.topic
     rospy.loginfo("Publishing the following command:")
     print cmd
-    pub.publish(cmd)
+
+    while(not rospy.is_shutdown()):
+	    pub.publish(cmd)
+            rospy.sleep(0.05)
+
     rospy.loginfo("Done publishing")
 
-    rospy.sleep(2.0)
 
 if __name__ == "__main__":
     main()
