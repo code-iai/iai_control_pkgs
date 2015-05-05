518c893587ffbd0bc12d010aa54fda4ce19430d4
Alexis Maldonado <amaldo@cs.uni-bremen.de>
2015-05-05 05:00:15 +0200

    Added watchdog to the joint controller

--- iai_control_utils/src/iai_control_utils/jcontroller.py
+++ iai_control_utils/src/iai_control_utils/jcontroller.py
@@ -1,3 +1,4 @@
+#
 #    Simple boxy joint controller
 #
 #    Copyright (c) 2014 Universitaet Bremen - Institute for Artificial Intelligence (Prof. Beetz)
@@ -29,26 +30,27 @@ from std_msgs.msg import Float32MultiArray
 from iai_control_msgs.msg import MultiJointVelocityImpedanceCommand
 from iai_control_msgs.msg import MultiJointVelocityCommand
 
-        
-        
+
+
 class JController(object):
     """Simple Position joint-array controller (Output: Velocity)"""
     def __init__(self, vel_output_topic_type, max_speed, ctrl_p, def_stiffness):
-        
+
         #Variable that checks if we can start working
         self.configured = False
-        
+
         #Will only start controlling after receiving at least one desired position
         self.received_des_pos_values = False
         self.def_stiffness = def_stiffness
-	
+        self.def_damping = 0.7
+
         #Controller parameters
         self.ctrl_p = ctrl_p
         self.max_speed = max_speed
-        
+
         self.num_dof = 0;
         self.init_work_variables()
-        
+
         #Chose the appropriate topic type
         if (vel_output_topic_type == 'MultiJointVelocityImpedanceCommand'):
             self.vel_output_topic_type = MultiJointVelocityImpedanceCommand
@@ -61,15 +63,16 @@ class JController(object):
         self.vel_pub = rospy.Publisher("~vel_command_out", numpy_msg(self.vel_output_topic_type), queue_size=3, tcp_nodelay=True, latch=False)   
         self.js_sub = rospy.Subscriber("~joint_states", numpy_msg(JointState), self.js_cb, queue_size=3, tcp_nodelay=True)
         self.js_des_pos = rospy.Subscriber("~pos_command_in", numpy_msg(Float32MultiArray), self.des_pos_cb, queue_size=3, tcp_nodelay=True)
-            
-        
-        
+
+
+
     def init_work_variables(self):
         self.des_pos = numpy.array([0.0] * self.num_dof, dtype=numpy.float32)      
         self.actual_pos = numpy.array([0.0] * self.num_dof, dtype=numpy.float32)
         self.error = numpy.array([0.0] * self.num_dof, dtype=numpy.float32)
-        
-        
+        self.last_command_time = rospy.Time()
+
+
     def set_num_dof(self, num_dof):
         if not self.configured:
             self.num_dof = num_dof
@@ -77,8 +80,8 @@ class JController(object):
             self.configured = True
         else:
             rospy.logerr('%s: Tried to change number of DOF, will ignore.' %(rospy.get_name()))
-        
-        
+
+
     def stop(self):
         ''''Stop the joint controller. Send zero velocities before going out. The watchdog should kick in, but just in case'''
         rospy.loginfo('%s: Stopping the P controller. Sending a last velocity command of zero.' %(rospy.get_name()))
@@ -89,12 +92,12 @@ class JController(object):
         out_msg.velocity = numpy.array([0.0] * self.num_dof, dtype=numpy.float32)
         out_msg.header.stamp = rospy.Time.now()
         self.vel_pub.publish(out_msg)
-        
+
         #Sleep a little to try to get that last message published before dying
         rospy.sleep(0.5)
         self.vel_pub.unregister()  #will happen when the node is dying anyway
-        
-        
+
+
     def js_cb(self, msg):
         '''Callback function that receives the current state of the joints.
            The first time it is called, it configures the number of DOF.
@@ -105,51 +108,58 @@ class JController(object):
             num_dof = len(msg.name)
             rospy.loginfo('%s: Setting Num DOF to %d' % (rospy.get_name(), num_dof))
             self.set_num_dof(num_dof)
-            
+
         #check length
         if (len(msg.position) == self.num_dof):
             #print type(msg.position)
             self.actual_pos = msg.position
             self.control_tick()
-            
+
     def control_tick(self):
         '''Do one iteration of the controller. For now, a simple P controller'''
-        
+
+        if ( (rospy.Time.now() - self.last_command_time) > rospy.Duration(0.1)):
+            return
+
         #Only work when the necessary information is available
         if self.received_des_pos_values and self.configured:
             self.error = self.des_pos - self.actual_pos
             vel_out = self.error * self.ctrl_p
             vel_out = numpy.clip(vel_out, -self.max_speed, self.max_speed)
-            
+
         else:
+            return
             #If not ready, just transmit zero velocities
-            vel_out = numpy.array([0.0] * self.num_dof, dtype=numpy.float32)
+            #vel_out = numpy.array([0.0] * self.num_dof, dtype=numpy.float32)
 
 
         if (self.vel_pub.get_num_connections() > 0):
             #Publish the message
             out_msg = self.vel_output_topic_type()
             out_msg.velocity = vel_out
-	    
-            if (self.vel_output_topic_type() == 'MultiJointVelocityImpedanceCommand'):
+
+            if (self.vel_output_topic_type is MultiJointVelocityImpedanceCommand):
                 out_msg.stiffness = [self.def_stiffness] * self.num_dof
-	    out_msg.header.stamp = rospy.Time.now()
+                out_msg.damping = [self.def_damping] * self.num_dof
+            out_msg.header.stamp = rospy.Time.now()
             self.vel_pub.publish(out_msg)
-            
-            
-            
+
+
+
     def des_pos_cb(self, msg):
         '''Callback function that received the desired joint positions for the controller'''
         #FIXME: More sanity checks on the input data?
-        
+
         if len(msg.data) == self.num_dof:
             #print type(msg.data)
+            #print msg.data
             self.des_pos = msg.data
-            
+            self.last_command_time = rospy.Time.now()
+
             if not self.received_des_pos_values:
-                        rospy.loginfo('%s: Received first joint position goal. Will start working' %(rospy.get_name()))
-                        self.received_des_pos_values = True
-                                
-            
+                rospy.loginfo('%s: Received first joint position goal. Will start working' %(rospy.get_name()))
+                self.received_des_pos_values = True
+
+
         else:
             rospy.logwarn('%s: Received desired position with non-matching number of joints. Ignoring.' % (rospy.get_name()))
